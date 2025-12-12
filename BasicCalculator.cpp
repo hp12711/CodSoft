c0cb8f569abfd0d609008e162bb74e4001776277
hp12711 <121652582+hp12711@users.noreply.github.com>
2025-12-13 03:36:37 +0530

    Implement basic calculator functionality

---
+++ BasicCalculator.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2;
+    char operation;
+
+    // Input two numbers
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    // Choose operation
+    cout << "Choose an operation (+, -, *, /): ";
+    cin >> operation;
+
+    // Perform calculation
+    switch (operation) {
+        case '+':
+            cout << "Result = " << num1 + num2;
+            break;
+
+        case '-':
+            cout << "Result = " << num1 - num2;
+            break;
+
+        case '*':
+            cout << "Result = " << num1 * num2;
+            break;
+
+        case '/':
+            if (num2 != 0)
+                cout << "Result = " << num1 / num2;
+            else
+                cout << "Error: Division by zero is not allowed.";
+            break;
+
+        default:
+            cout << "Invalid operation selected.";
+    }
+
+    return 0;
+}
